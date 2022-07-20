f7d3f2cfa22e51f3cd8850eb312f6ff8dba4b096
iff-padua-ctic <ctic.padua@iff.edu.br>
2022-07-20 10:29:07 -0300

    commit

--- app-entrypoint.sh
+++ app-entrypoint.sh
@@ -10,7 +10,6 @@ php artisan route:clear
 php artisan config:clear
 php artisan view:clear
 cp /home/.env /var/www/html/.env
-sleep 30 &&
 php artisan migrate
 php artisan db:seed --class=AdminSeeder 
 php artisan db:seed --class=CategoriasSeeder
