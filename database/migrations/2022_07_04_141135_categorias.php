2b2180eaae5e9c8392cba4b84f6c4a7ef34f2916
iff-padua-ctic <ctic.padua@iff.edu.br>
2022-07-21 13:52:18 -0300

    alterado migration categorias, campo nome da tabela categorias definida como unica

--- database/migrations/2022_07_04_141135_categorias.php
+++ database/migrations/2022_07_04_141135_categorias.php
@@ -15,7 +15,7 @@ class Categorias extends Migration
     {
         Schema::create('categorias', function (Blueprint $table) {
             $table->id();
-            $table->string('nome');
+            $table->string('nome')->unique();
             $table->timestamps();
         });  //
         //
