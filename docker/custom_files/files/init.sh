a6ea26b7db942a5f80a25fc9755486b7abf95bce
iff-padua-ctic <ctic.padua@iff.edu.br>
2022-08-04 08:26:53 -0300

    commit

--- docker/custom_files/files/init.sh
+++ docker/custom_files/files/init.sh
@@ -10,6 +10,7 @@ echo "DB_HOST=$DB_HOST" >> .env
 echo "DB_PORT=$DB_PORT" >> .env
 echo "DB_DATABASE=$DB_DATABASE" >> .env
 echo "DB_USERNAME=$DB_USERNAME" >> .env
+echo "DB_PASSWORD"=$DB_PASSWORD >> .env
 
 
 composer install --optimize-autoloader --no-dev
